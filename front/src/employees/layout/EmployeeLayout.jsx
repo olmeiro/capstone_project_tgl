f1ca0ef100bc80ec3b4434d9500ae0f5a5b83ef7
Olmeiro orozco <olme.orozco@gmail.com>
2022-07-11 11:07:02 -0500

    NavBar Component

--- front/src/employees/layout/EmployeeLayout.jsx
+++ front/src/employees/layout/EmployeeLayout.jsx
@@ -1,16 +1,19 @@
-import { Box } from '@mui/system'
-import React from 'react'
+import React from "react"
+import { Box } from "@mui/system"
+
+import { NavBar } from "../components"
+
+const drawerWidth = 240
 
 export const EmployeeLayout = ({ children }) => {
   return (
-    <Box sx={{ display: 'flex'}}>
-      {/* Navbar */}
-      {/* Sidebar */}
+    <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
 
-      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
+        {children}
       </Box>
-      { children }
     </Box>
   )
 }
--- front/src/employees/pages/EmployeesPage.jsx
+++ front/src/employees/pages/EmployeesPage.jsx
@@ -1,13 +1,15 @@
-import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
 import React from "react"
+import { EmployeeLayout } from "../layout/EmployeeLayout"
 
 export const EmployeesPage = () => {
   return (
-    <>
-    <Typography variant="h1">EmployeesPage</Typography>
-    <MailOutline />
-    </>
+    <EmployeeLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim non maiores tempora, ea illo quae iusto assumenda atque reprehenderit, est vel ipsum! Illo eius minus animi dolore architecto assumenda neque.</Typography>
+
+      {/* NothingSelected */}
+      {/* ListEmployees */}
+    </EmployeeLayout>
   ) 
   
 }
