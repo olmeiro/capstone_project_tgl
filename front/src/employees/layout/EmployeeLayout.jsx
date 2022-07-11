5bf95fa080eb50ac11efc585bbd4996bfcee840a
Olmeiro orozco <olme.orozco@gmail.com>
2022-07-11 11:25:49 -0500

    Sidebar desing

--- front/src/employees/layout/EmployeeLayout.jsx
+++ front/src/employees/layout/EmployeeLayout.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 import { Box } from "@mui/system"
 
-import { NavBar } from "../components"
+import { NavBar, SideBar } from "../components"
+import { Toolbar } from "@mui/material"
 
-const drawerWidth = 240
+const drawerWidth = 280
 
 export const EmployeeLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWidth={drawerWidth} />
-
+      <SideBar drawerWidth={drawerWidth} /> 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/* Toolbar */}
+        <Toolbar />
         {children}
       </Box>
     </Box>
